killbill/killbill-client-java

commit 9a507c14b643fba1d011b93f6e0aa9c1a6781d75

Add ability to post and retrieve per tenant catalog and overdue config.

src/main/java/org/killbill/billing/client/KillBillHttpClient.java
@@ -66,6 +66,8 @@ public class KillBillHttpClient {
     public static final String RBAC_OPTION_USERNAME = "__RBAC_OPTION_USERNAME";
     public static final String RBAC_OPTION_PASSWORD = "__RBAC_OPTION_PASSWORD";
 
+    public static final String HTPP_HEADER_CONTENT_TYPE = "Content-Type";
+
     private static final Logger log = LoggerFactory.getLogger(KillBillHttpClient.class);
     private static final String USER_AGENT = "KillBill-JavaClient/1.0";
 
@@ -294,6 +296,7 @@ public class KillBillHttpClient {
         options.removeAll(RBAC_OPTION_USERNAME);
         options.removeAll(RBAC_OPTION_PASSWORD);
 
+
         final BoundRequestBuilder builder = getBuilderWithHeaderAndQuery(verb, getKBServerUrl(uri), username, password, options);
 
         // Multi-Tenancy headers
@@ -316,15 +319,20 @@ public class KillBillHttpClient {
 
         if (!"GET".equals(verb) && !"HEAD".equals(verb)) {
             if (body != null) {
-                try {
-                    builder.setBody(mapper.writeValueAsString(body));
-                } catch (JsonProcessingException e) {
-                    throw new KillBillClientException(e);
+                if (body instanceof String) {
+                    builder.setBody((String) body);
+                } else {
+                    try {
+                        builder.setBody(mapper.writeValueAsString(body));
+                    } catch (JsonProcessingException e) {
+                        throw new KillBillClientException(e);
+                    }
                 }
-            } else {
-                builder.setBody("{}");
             }
+        } else {
+            builder.setBody("{}");
         }
+
 
         if (followLocation) {
             final Response response = executeAndWait(builder, timeoutSec, Response.class);
@@ -483,7 +491,14 @@ public class KillBillHttpClient {
             builder.setRealm(realm);
         }
 
-        builder.addHeader("Content-Type", "application/json; charset=utf-8");
+        String contentType = getUniqueValue(options, HTPP_HEADER_CONTENT_TYPE);
+        if (contentType == null) {
+            contentType = "application/json; charset=utf-8";
+        } else {
+            options.removeAll(HTPP_HEADER_CONTENT_TYPE);
+        }
+        builder.addHeader(HTPP_HEADER_CONTENT_TYPE, contentType);
+
         builder.setBodyEncoding("UTF-8");
 
         for (final String key : options.keySet()) {

src/main/java/org/killbill/billing/client/JaxrsResource.java
@@ -53,6 +53,7 @@ public interface JaxrsResource {
     public static final String INVOICE_PAYMENTS_PATH = PREFIX + "/" + INVOICE_PAYMENTS;
     public static final String NOTIFICATION = "notification";
     public static final String OVERDUE = "overdue";
+    public static final String OVERDUE_PATH = PREFIX + "/" + OVERDUE;
     public static final String PAGINATION = "pagination";
     public static final String PAYMENTS = "payments";
     public static final String PAYMENTS_PATH = PREFIX + "/" + PAYMENTS;

src/main/java/org/killbill/billing/client/KillBillClient.java
@@ -18,7 +18,10 @@
 
 package org.killbill.billing.client;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -82,6 +85,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import com.google.common.io.Files;
 
 import static org.killbill.billing.client.KillBillHttpClient.DEFAULT_EMPTY_QUERY;
 import static org.killbill.billing.client.KillBillHttpClient.DEFAULT_HTTP_TIMEOUT_SEC;
@@ -1082,6 +1086,39 @@ public class KillBillClient {
 
     // Overdue
 
+
+    public void uploadXMLOverdueConfig(final String overdueConfigPath, final String createdBy, final String reason, final String comment) throws KillBillClientException {
+        Preconditions.checkNotNull(overdueConfigPath, "overdueConfigPath cannot be null");
+
+        final String uri = JaxrsResource.OVERDUE_PATH;
+
+        final Multimap<String, String> queryParams = paramsWithAudit(createdBy, reason, comment);
+        queryParams.put(KillBillHttpClient.HTPP_HEADER_CONTENT_TYPE, "application/xml");
+
+        final File catalogFile = new File(overdueConfigPath);
+        Preconditions.checkArgument(catalogFile.exists() && catalogFile.isFile() && catalogFile.canRead(), "overdueConfigPath needs to be a valid file");
+        try {
+            final String body = Files.toString(catalogFile, Charset.forName("UTF-8"));
+            httpClient.doPost(uri, body, queryParams);
+        } catch (IOException e) {
+            throw new KillBillClientException(e);
+        }
+    }
+
+    public String getXMLOverdueConfig() throws KillBillClientException {
+        final String uri = JaxrsResource.OVERDUE_PATH;
+
+        final Multimap<String, String> queryParams = HashMultimap.create();
+        queryParams.put(KillBillHttpClient.HTPP_HEADER_CONTENT_TYPE, "application/xml");
+
+        final Response response = httpClient.doGet(uri, queryParams);
+        try {
+            return response.getResponseBody("UTF-8");
+        } catch (IOException e) {
+            throw new KillBillClientException(e);
+        }
+    }
+
     public OverdueState getOverdueStateForAccount(final UUID accountId) throws KillBillClientException {
         final String uri = JaxrsResource.ACCOUNTS_PATH + "/" + accountId + "/" + JaxrsResource.OVERDUE;
 
@@ -1293,6 +1330,40 @@ public class KillBillClient {
         return httpClient.doGet(uri, DEFAULT_EMPTY_QUERY, PlanDetails.class);
     }
 
+    public void uploadXMLCatalog(final String catalogPath, final String createdBy, final String reason, final String comment) throws KillBillClientException {
+        Preconditions.checkNotNull(catalogPath, "catalogPath cannot be null");
+
+        final String uri = JaxrsResource.CATALOG_PATH;
+
+        final Multimap<String, String> queryParams = paramsWithAudit(createdBy, reason, comment);
+        queryParams.put(KillBillHttpClient.HTPP_HEADER_CONTENT_TYPE, "application/xml");
+
+        final File catalogFile = new File(catalogPath);
+        Preconditions.checkArgument(catalogFile.exists() && catalogFile.isFile() && catalogFile.canRead(), "catalogPath needs to be a valid file");
+        try {
+            final String body = Files.toString(catalogFile, Charset.forName("UTF-8"));
+            httpClient.doPost(uri, body, queryParams);
+        } catch (IOException e) {
+            throw new KillBillClientException(e);
+        }
+    }
+
+    public String getXMLCatalog() throws KillBillClientException {
+        final String uri = JaxrsResource.CATALOG_PATH;
+
+        final Multimap<String, String> queryParams = HashMultimap.create();
+        queryParams.put(KillBillHttpClient.HTPP_HEADER_CONTENT_TYPE, "application/xml");
+
+        final Response response = httpClient.doGet(uri, queryParams);
+        try {
+            return response.getResponseBody("UTF-8");
+        } catch (IOException e) {
+            throw new KillBillClientException(e);
+        }
+    }
+
+
+
     // Tenants
 
     public Tenant createTenant(final Tenant tenant, final String createdBy, final String reason, final String comment) throws KillBillClientException {
@@ -1379,9 +1450,11 @@ public class KillBillClient {
     }
 
     private Multimap<String, String> paramsWithAudit(final String createdBy, final String reason, final String comment) {
-        return ImmutableMultimap.<String, String>of(KillBillHttpClient.AUDIT_OPTION_CREATED_BY, createdBy,
-                                                    KillBillHttpClient.AUDIT_OPTION_REASON, reason,
-                                                    KillBillHttpClient.AUDIT_OPTION_COMMENT, comment);
+        final Multimap result = HashMultimap.create();
+        result.put(KillBillHttpClient.AUDIT_OPTION_CREATED_BY, createdBy);
+        result.put(KillBillHttpClient.AUDIT_OPTION_REASON, reason);
+        result.put(KillBillHttpClient.AUDIT_OPTION_COMMENT, comment);
+        return result;
     }
 
     private void storePluginPropertiesAsParams(final Map<String, String> pluginProperties, final Multimap<String, String> params) {

src/main/java/org/killbill/billing/client/model/OverdueState.java
@@ -16,6 +16,8 @@
 
 package org.killbill.billing.client.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -23,7 +25,7 @@ public class OverdueState extends KillBillObject {
 
     private String name;
     private String externalMessage;
-    private Integer daysBetweenPaymentRetries;
+    private List<Integer> daysBetweenPaymentRetries;
     private Boolean disableEntitlementAndChangesBlocked;
     private Boolean blockChanges;
     private Boolean isClearState;
@@ -32,7 +34,7 @@ public class OverdueState extends KillBillObject {
     @JsonCreator
     public OverdueState(@JsonProperty("name") final String name,
                         @JsonProperty("externalMessage") final String externalMessage,
-                        @JsonProperty("daysBetweenPaymentRetries") final Integer daysBetweenPaymentRetries,
+                        @JsonProperty("daysBetweenPaymentRetries") final List<Integer> daysBetweenPaymentRetries,
                         @JsonProperty("disableEntitlementAndChangesBlocked") final Boolean disableEntitlementAndChangesBlocked,
                         @JsonProperty("blockChanges") final Boolean blockChanges,
                         @JsonProperty("clearState") final Boolean isClearState,
@@ -62,11 +64,11 @@ public class OverdueState extends KillBillObject {
         this.externalMessage = externalMessage;
     }
 
-    public Integer getDaysBetweenPaymentRetries() {
+    public List<Integer> getDaysBetweenPaymentRetries() {
         return daysBetweenPaymentRetries;
     }
 
-    public void setDaysBetweenPaymentRetries(final Integer daysBetweenPaymentRetries) {
+    public void setDaysBetweenPaymentRetries(final List<Integer> daysBetweenPaymentRetries) {
         this.daysBetweenPaymentRetries = daysBetweenPaymentRetries;
     }
 

commit 5c21fc5f16927a0282bdda81f50d250a38100259

Fix regression introduced in 9a507c14b643fba1d011b93f6e0aa9c1a6781d75

src/main/java/org/killbill/billing/client/KillBillHttpClient.java
@@ -328,9 +328,9 @@ public class KillBillHttpClient {
                         throw new KillBillClientException(e);
                     }
                 }
+            } else {
+                builder.setBody("{}");
             }
-        } else {
-            builder.setBody("{}");
         }
 
 