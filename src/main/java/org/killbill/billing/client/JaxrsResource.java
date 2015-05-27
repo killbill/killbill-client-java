dcd7b9d19e46220f10d8761ff4f38d79a7c8293a
Alexander Kley <akley@users.noreply.github.com>
2015-05-27 08:32:11 +0200

    Update JaxrsResource.java

--- src/main/java/org/killbill/billing/client/JaxrsResource.java
+++ src/main/java/org/killbill/billing/client/JaxrsResource.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2014 Groupon, Inc
  * Copyright 2014 The Billing Project, LLC
+ * Copyright 2015 Cloudyle GmbH
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
