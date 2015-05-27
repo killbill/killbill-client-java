68e38eb54e1cbf8e7fa76764adc48bcfbb8a6da9
Alexander Kley <akley@users.noreply.github.com>
2015-05-27 08:32:49 +0200

    Update KillBillClient.java

--- src/main/java/org/killbill/billing/client/KillBillClient.java
+++ src/main/java/org/killbill/billing/client/KillBillClient.java
@@ -2,6 +2,7 @@
  * Copyright 2010-2013 Ning, Inc.
  * Copyright 2014 Groupon, Inc
  * Copyright 2014 The Billing Project, LLC
+ * Copyright 2015 Cloudyle GmbH
  *
  * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
