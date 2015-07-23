b91a6681584952056c9313ed391b0d59b35e91c3
stephane brossier <sbrossier@groupon.com>
2015-07-22 21:28:46 -0700

    Add plugin properties per operations in payment combo call

--- src/main/java/org/killbill/billing/client/model/ComboPaymentTransaction.java
+++ src/main/java/org/killbill/billing/client/model/ComboPaymentTransaction.java
@@ -18,6 +18,8 @@
 
 package org.killbill.billing.client.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -26,17 +28,24 @@ public class ComboPaymentTransaction extends KillBillObject {
     private Account account;
     private PaymentMethod paymentMethod;
     private PaymentTransaction transaction;
+    private List<PluginProperty> paymentMethodPluginProperties;
+    private List<PluginProperty> transactionPluginProperties;
+
 
     public ComboPaymentTransaction() { }
 
     @JsonCreator
     public ComboPaymentTransaction(@JsonProperty("account") final Account account,
                                    @JsonProperty("paymentMethod") final PaymentMethod paymentMethod,
-                                   @JsonProperty("transaction") final PaymentTransaction transaction) {
+                                   @JsonProperty("transaction") final PaymentTransaction transaction,
+                                   @JsonProperty("paymentMethodPluginProperties") final List<PluginProperty> paymentMethodPluginProperties,
+                                   @JsonProperty("transactionPluginProperties") final List<PluginProperty> transactionPluginProperties) {
         super(null);
         this.account = account;
         this.paymentMethod = paymentMethod;
         this.transaction = transaction;
+        this.paymentMethodPluginProperties = paymentMethodPluginProperties;
+        this.transactionPluginProperties = transactionPluginProperties;
     }
 
     public Account getAccount() {
@@ -51,12 +60,22 @@ public class ComboPaymentTransaction extends KillBillObject {
         return transaction;
     }
 
+    public List<PluginProperty> getPaymentMethodPluginProperties() {
+        return paymentMethodPluginProperties;
+    }
+
+    public List<PluginProperty> getTransactionPluginProperties() {
+        return transactionPluginProperties;
+    }
+
     @Override
     public String toString() {
         return "ComboPaymentTransaction{" +
                "account=" + account +
                ", paymentMethod=" + paymentMethod +
                ", transaction=" + transaction +
+               ", paymentMethodPluginProperties=" + paymentMethodPluginProperties +
+               ", transactionPluginProperties=" + transactionPluginProperties +
                '}';
     }
 
@@ -77,7 +96,13 @@ public class ComboPaymentTransaction extends KillBillObject {
         if (paymentMethod != null ? !paymentMethod.equals(that.paymentMethod) : that.paymentMethod != null) {
             return false;
         }
-        return !(transaction != null ? !transaction.equals(that.transaction) : that.transaction != null);
+        if (transaction != null ? !transaction.equals(that.transaction) : that.transaction != null) {
+            return false;
+        }
+        if (paymentMethodPluginProperties != null ? !paymentMethodPluginProperties.equals(that.paymentMethodPluginProperties) : that.paymentMethodPluginProperties != null) {
+            return false;
+        }
+        return !(transactionPluginProperties != null ? !transactionPluginProperties.equals(that.transactionPluginProperties) : that.transactionPluginProperties != null);
 
     }
 
@@ -86,6 +111,8 @@ public class ComboPaymentTransaction extends KillBillObject {
         int result = account != null ? account.hashCode() : 0;
         result = 31 * result + (paymentMethod != null ? paymentMethod.hashCode() : 0);
         result = 31 * result + (transaction != null ? transaction.hashCode() : 0);
+        result = 31 * result + (paymentMethodPluginProperties != null ? paymentMethodPluginProperties.hashCode() : 0);
+        result = 31 * result + (transactionPluginProperties != null ? transactionPluginProperties.hashCode() : 0);
         return result;
     }
 }
