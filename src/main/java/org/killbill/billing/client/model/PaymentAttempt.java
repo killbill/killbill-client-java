1974fc09db44deaa5e37140066e636677a55e5b8
Javier Gómez <jgomez@velocitypartners.net>
2016-07-04 14:57:45 -0300

    #69 - Fixed license declaration

--- src/main/java/org/killbill/billing/client/model/PaymentAttempt.java
+++ src/main/java/org/killbill/billing/client/model/PaymentAttempt.java
@@ -1,7 +1,7 @@
 /*
- * Copyright 2014 Groupon, Inc
+ * Copyright 2016 The Billing Project, LLC
  *
- * Groupon licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -12,7 +12,7 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
- */
+*/
 
 package org.killbill.billing.client.model;
 
