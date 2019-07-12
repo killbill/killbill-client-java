d7aac15db8d3aa27665f2d6450a9ccfc92cc8c31
stephane brossier <sbrossier@groupon.com>
2019-07-12 16:34:12 -0700

    Subscription#getSubscriptionByKey. See See killbill/killbill#1157

--- src/main/java/org/killbill/billing/client/api/gen/SubscriptionApi.java
+++ src/main/java/org/killbill/billing/client/api/gen/SubscriptionApi.java
@@ -419,6 +419,35 @@ public class SubscriptionApi {
         return httpClient.doGet(uri, AuditLogs.class, requestOptions);
     }
 
+    public Subscription getSubscriptionByKey(final String externalKey, final UUID accountId, final RequestOptions inputOptions) throws KillBillClientException {
+        return getSubscriptionByKey(externalKey, accountId, AuditLevel.NONE, inputOptions);
+    }
+
+    public Subscription getSubscriptionByKey(final String externalKey, final UUID accountId, final AuditLevel audit, final RequestOptions inputOptions) throws KillBillClientException {
+        Preconditions.checkNotNull(externalKey, "Missing the required parameter 'externalKey' when calling getSubscriptionByKey");
+        Preconditions.checkNotNull(accountId, "Missing the required parameter 'accountId' when calling getSubscriptionByKey");
+
+        final String uri = "/1.0/kb/subscriptions";
+
+        final Multimap<String, String> queryParams = LinkedListMultimap.create(inputOptions.getQueryParams());
+        if (externalKey != null) {
+            queryParams.put("externalKey", String.valueOf(externalKey));
+        }
+        if (accountId != null) {
+            queryParams.put("accountId", String.valueOf(accountId));
+        }
+        if (audit != null) {
+            queryParams.put("audit", String.valueOf(audit));
+        }
+
+        final RequestOptionsBuilder inputOptionsBuilder = inputOptions.extend();
+        inputOptionsBuilder.withQueryParams(queryParams);
+        inputOptionsBuilder.withHeader(KillBillHttpClient.HTTP_HEADER_ACCEPT, "application/json");
+        final RequestOptions requestOptions = inputOptionsBuilder.build();
+
+        return httpClient.doGet(uri, Subscription.class, requestOptions);
+    }
+
     public CustomFields getSubscriptionCustomFields(final UUID subscriptionId, final RequestOptions inputOptions) throws KillBillClientException {
         return getSubscriptionCustomFields(subscriptionId, AuditLevel.NONE, inputOptions);
     }
